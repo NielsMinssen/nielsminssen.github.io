43458c7e5961fd23648219adb6602d2efe324d09
Minssen <niels.minssen@etu.uca.fr>
2022-05-23 00:28:51 +0200

    Updates

---
+++ precache-manifest.4408f80679906d206674f7d966c5ae32.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f711abeddd1a0dc8f8016885ad690f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeaa86992896fd748344",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "8070503b9aae74865f05",
+    "url": "/static/js/2.b86c7e36.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.b86c7e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeaa86992896fd748344",
+    "url": "/static/js/main.c7bbf103.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "f2d0fcaf3473afaa5a1279946b8e851a",
+    "url": "/static/media/IMG-20210909-WA0000.f2d0fcaf.jpg"
+  },
+  {
+    "revision": "a6c96c393f0a96954efc9be34f3ba5ba",
+    "url": "/static/media/cv_niels_minssen.a6c96c39.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/precache-manifest.8ddcebda1905aa83bd270f02e43c58cc.js"
+  "/precache-manifest.4408f80679906d206674f7d966c5ae32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
